84139f096ad80c6aeabe35402412209698a042ea
Oli Larkin <olilarkin@googlemail.com>
2016-06-09 09:54:50 +0100

    uicomponent wip from qtui

--- source/FaustUIComponent.cpp
+++ source/FaustUIComponent.cpp
@@ -10,12 +10,41 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "FaustUIComponent.h"
 
+std::list<GUI*> GUI::fGuiList;
+
+FaustSlider::FaustSlider(const char* label, GUI* ui, FAUSTFLOAT* zone, FAUSTFLOAT cur, FAUSTFLOAT lo, FAUSTFLOAT hi, FAUSTFLOAT step, Scale scale)
+: Slider(String(label))
+, uiItem(ui, zone)
+, fCur(cur), fMin(lo), fMax(hi), fStep(step)
+{
+  setSliderStyle(LinearHorizontal);
+  
+  if (scale == kLog)
+    fConverter = new LogValueConverter(0., 1., fMin, fMax);
+  else if (scale == kExp)
+    fConverter = new ExpValueConverter(0., 1., fMin, fMax);
+  else
+    fConverter = new LinearValueConverter(0, 1., fMin, fMax);
+  
+  setRange(fMin, fMax);
+  setValue(fConverter->faust2ui(fCur));
+  *fZone = fCur;
+}
+
+void FaustSlider::reflectZone()
+{
+  FAUSTFLOAT v = *fZone;
+  fCache = v;
+  setValue(fConverter->faust2ui(v));
+}
+
 FaustUIComponent::FaustUIComponent()
 {
 }
 
 FaustUIComponent::~FaustUIComponent()
 {
+  deleteAllChildren();
 }
 
 void FaustUIComponent::paint (Graphics& g)
@@ -24,6 +53,15 @@ void FaustUIComponent::paint (Graphics& g)
 
 void FaustUIComponent::resized()
 {
+  int width = getWidth();
+  int height = getHeight();
+  
+  int hdiv = height / getNumChildComponents();
+  
+  for(int i = 0; i < getNumChildComponents(); i++)
+  {
+    getChildComponent(i)->setBounds(0, i * hdiv, width, (i + 1) * hdiv);
+  }
 }
 
 #pragma mark Faust UI
@@ -59,12 +97,12 @@ void FaustUIComponent::addCheckButton(const char* label, FAUSTFLOAT* zone)
 
 void FaustUIComponent::addVerticalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step)
 {
-  addAndMakeVisible(new Slider(String(label)));
+  addAndMakeVisible(new FaustSlider(label, this, zone, init, min, max, step, getScale(zone)));
 }
 
 void FaustUIComponent::addHorizontalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step)
 {
-  addAndMakeVisible(new Slider(String(label)));
+  addAndMakeVisible(new FaustSlider(label, this, zone, init, min, max, step, getScale(zone)));
 }
 
 void FaustUIComponent::addNumEntry(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step)
@@ -84,47 +122,47 @@ void FaustUIComponent::addVerticalBargraph(const char* label, FAUSTFLOAT* zone, 
 
 void FaustUIComponent::declare(FAUSTFLOAT* zone, const char* key, const char* value)
 {
-  //  if (zone == 0) {
-  //    // special zone 0 means group metadata
-  //    if (strcmp(key, "tooltip")==0) {
-  //      // only group tooltip are currently implemented
-  //      gGroupTooltip = formatTooltip(30, value);
-  //    }
-  //  } else {
-  //    if (strcmp(key, "size") == 0) {
-  //      fGuiSize[zone]=atof(value);
-  //    }
-  //    else if (strcmp(key, "tooltip") == 0) {
-  //      fTooltip[zone] = formatTooltip(30, value) ;
-  //    }
-  //    else if (strcmp(key, "unit") == 0) {
-  //      fUnit[zone] = value ;
-  //    }
-  //    else if (strcmp(key, "scale") == 0) {
-  //      if (strcmp(value, "log") == 0) {
-  //        fLogSet.insert(zone);
-  //      } else if (strcmp(value, "exp") == 0) {
-  //        fExpSet.insert(zone);
-  //      }
-  //    }
-  //    else if (strcmp(key, "style") ==0) {
-  //      // else if ((strcmp(key,"style")==0) || (strcmp(key,"type")==0)) {
-  //      if (strcmp(value, "knob") == 0) {
-  //        fKnobSet.insert(zone);
-  //      } else if (strcmp(value, "led") == 0) {
-  //        fLedSet.insert(zone);
-  //      } else if (strcmp(value, "numerical") == 0) {
-  //        fNumSet.insert(zone);
-  //      } else {
-  //        const char* p = value;
-  //        if (parseWord(p, "radio")) {
-  //          fRadioDescription[zone] = string(p);
-  //        } else if (parseWord(p, "menu")) {
-  //          fMenuDescription[zone] = string(p);
-  //        }
-  //      }
-  //    }
-  //  }
+  if (zone == 0) {
+    // special zone 0 means group metadata
+    if (strcmp(key, "tooltip")==0) {
+      // only group tooltips are currently implemented
+      gGroupTooltip = formatTooltip(30, value);
+    }
+  } else {
+    if (strcmp(key, "size") == 0) {
+      fGuiSize[zone]=atof(value);
+    }
+    else if (strcmp(key, "tooltip") == 0) {
+      fTooltip[zone] = formatTooltip(30, value) ;
+    }
+    else if (strcmp(key, "unit") == 0) {
+      fUnit[zone] = value ;
+    }
+    else if (strcmp(key, "scale") == 0) {
+      if (strcmp(value, "log") == 0) {
+        fLogSet.insert(zone);
+      } else if (strcmp(value, "exp") == 0) {
+        fExpSet.insert(zone);
+      }
+    }
+    else if (strcmp(key, "style") ==0) {
+      // else if ((strcmp(key,"style")==0) || (strcmp(key,"type")==0)) {
+      if (strcmp(value, "knob") == 0) {
+        fKnobSet.insert(zone);
+      } else if (strcmp(value, "led") == 0) {
+        fLedSet.insert(zone);
+      } else if (strcmp(value, "numerical") == 0) {
+        fNumSet.insert(zone);
+      } else {
+//        const char* p = value;
+//        if (parseWord(p, "radio")) {
+//          fRadioDescription[zone] = string(p);
+//        } else if (parseWord(p, "menu")) {
+//          fMenuDescription[zone] = string(p);
+//        }
+      }
+    }
+  }
 }
 
 #pragma mark FAUST metadata declarations
--- source/FaustUIComponent.h
+++ source/FaustUIComponent.h
@@ -11,29 +11,35 @@
 #define FAUSTUICOMPONENT_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "faust/gui/UI.h"
+#include "faust/gui/GUI.h"
 #include "faust/gui/meta.h"
-
+#include "faust/gui/ValueConverter.h"
 
 class FaustSlider : public Slider
+                  , public uiItem
 {
 public:
-  FaustSlider(const juce::String& componentName)
+  enum Scale
   {
-  }
-protected:
-  void mouseDown(const MouseEvent& e)
-  {
-  }
+    kLin,
+    kLog,
+    kExp
+  };
   
-  void mouseUp(const MouseEvent& e)
-  {
-  }
+  FaustSlider(const char* label, GUI* ui, FAUSTFLOAT* zone, FAUSTFLOAT cur, FAUSTFLOAT lo, FAUSTFLOAT hi, FAUSTFLOAT step, Scale scale);
+
+  void reflectZone() override;
   
+private:
+  FAUSTFLOAT fCur;
+  FAUSTFLOAT fMin;
+  FAUSTFLOAT fMax;
+  FAUSTFLOAT fStep;
+  ScopedPointer<ValueConverter> fConverter;
 };
 
 class FaustUIComponent : public Component
-                       , public UI
+                       , public GUI
                        , public Meta
 {
 public:
@@ -63,7 +69,64 @@ public:
   // FAUST metadata declarations
   void declare(const char* key, const char* value) override;
   
+  FaustSlider::Scale getScale(FAUSTFLOAT* zone)
+  {
+    if (fLogSet.count(zone) > 0)
+      return FaustSlider::kLog;
+    if (fExpSet.count(zone) > 0)
+      return FaustSlider::kExp;
+    return FaustSlider::kLin;
+  }
+  
+  /**
+   * Format tooltip string by replacing some white spaces by
+   * return characters so that line width doesn't exceed n.
+   * Limitation : long words exceeding n are not cut
+   */
+  std::string formatTooltip(int n, const std::string& tt)
+  {
+    std::string  ss = tt;	// ss string we are going to format
+    int	lws = 0;	// last white space encountered
+    int 	lri = 0;	// last return inserted
+    for (int i=0; i< (int)tt.size(); i++) {
+      if (tt[i] == ' ') lws = i;
+      if (((i-lri) >= n) && (lws > lri)) {
+        // insert return here
+        ss[lws] = '\n';
+        lri = lws;
+      }
+    }
+    return ss;
+  }
+  
+  void clearMetadata()
+  {
+    fGuiSize.clear();
+    fTooltip.clear();
+    fUnit.clear();
+    fRadioDescription.clear();
+    fMenuDescription.clear();
+    fKnobSet.clear();
+    fLedSet.clear();
+    fNumSet.clear();
+    fLogSet.clear();
+    fExpSet.clear();
+  }
+  
 private:
+  std::map<FAUSTFLOAT*, FAUSTFLOAT>      fGuiSize;            // map widget zone with widget size coef
+  std::map<FAUSTFLOAT*, std::string>     fTooltip;            // map widget zone with tooltip strings
+  std::map<FAUSTFLOAT*, std::string>     fUnit;               // map widget zone to unit string (i.e. "dB")
+  std::map<FAUSTFLOAT*, std::string>     fRadioDescription;   // map zone to {'low':440; ...; 'hi':1000.0}
+  std::map<FAUSTFLOAT*, std::string>     fMenuDescription;    // map zone to {'low':440; ...; 'hi':1000.0}
+  std::set<FAUSTFLOAT*>                  fKnobSet;            // set of widget zone to be knobs
+  std::set<FAUSTFLOAT*>                  fLedSet;             // set of widget zone to be LEDs
+  std::set<FAUSTFLOAT*>                  fNumSet;             // set of widget zone to be numerical bargraphs
+  std::set<FAUSTFLOAT*>                  fLogSet;             // set of widget zone having a log UI scale
+  std::set<FAUSTFLOAT*>                  fExpSet;             // set of widget zone having an exp UI scale
+  
+  std::string gGroupTooltip;
+
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FaustUIComponent)
 };
 
